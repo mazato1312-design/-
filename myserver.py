f251b17d5b302b4bae9620a1068905b2a80d3c68
mazato1312-design <mazato1312@gmail.com>
2025-12-16 10:52:28 +0700

    Update myserver.py

--- myserver.py
+++ myserver.py
@@ -1,15 +1,22 @@
+import os
 from flask import Flask
 from threading import Thread
 
-app = Flask('')
+app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Server is running!"
+    return "I am alive"
 
 def run():
-    app.run(host='0.0.0.0',PORT=8080)
+    # IMPORTANT: You must use host='0.0.0.0' and the PORT env variable
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 8080)))
 
-def server_on():
+def keep_alive():
     t = Thread(target=run)
     t.start()
+
+# --- Your Bot Code Below ---
+# ...
+# keep_alive()  <-- Call this before you run your bot
+# client.run(TOKEN)
